107829483a074489e2fa0bce0f7bc46a9a162283
PolaDinitz <44160650+PolaDinitz@users.noreply.github.com>
2020-12-09 21:37:13 +0200

    init funcs

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+def minimum_spanning_tree():
+    return None
+
+
+def update_mininum_spanning_tree():
+    return None
+
+
+def print_minimum_spanning_tree():
+    return None
+
 
 def main():
     pass
